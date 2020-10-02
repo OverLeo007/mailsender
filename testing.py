34afe7d0b6482f9d34b7e632fbe0aa912ddccd59
Leo <57536503+OverLeo007@users.noreply.github.com>
2020-10-02 10:19:43 +0700

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj.starttls()
-smtpObj.login('Vaxxo9000@gmail.com', 'DuperSuper1337')
-smtpObj.sendmail("Vaxxo9000@gmail.com", "sokolov_lev2003@mail.ru", "go to bed!")
-smtpObj.quit()
